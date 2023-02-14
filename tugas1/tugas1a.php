5f490c23b44e3fc0cbb2c822abe9b3f33a0e69d1
Harmert <aryasaputra1304@gmail.com>
2023-02-14 15:09:58 -0800

    first commit

---
+++ tugas1/tugas1a.php
@@ -0,0 +1,19 @@
+<?php 
+    $benda = "Topi";
+    $bentuk = "Bundar";
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tugas 1a</title>
+</head>
+<body>
+    <h2>
+        <i> <?php echo " \"$benda saya $bentuk, $bentuk $benda saya\" " ?> </i>   
+    </h2>
+</body>
+</html>
